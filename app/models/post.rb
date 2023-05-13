fcf1e592dc6fbf8b16a25c8d795300ed0855638f
Donmark2k <chukwuemekavp@gmail.com>
2023-05-13 09:30:18 +0100

    modify index.html.erb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, foreign_key: :author_id, class_name: 'User', counter_cache: :postscounter
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  
   validates :title, presence: true, length: { maximum: 250 }
   validates :commentscounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :likescounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
+  after_save :update_user_post_counter
 
   def update_user_post_counter
     author.increment!(:postscounter)
