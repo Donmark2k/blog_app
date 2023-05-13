c11f3906482ce21f19981e19705d7f1476ca1699
Donmark2k <chukwuemekavp@gmail.com>
2023-05-13 10:00:48 +0100

    Solve linters error

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, foreign_key: :author_id, class_name: 'User', counter_cache: :postscounter
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  
+
   validates :title, presence: true, length: { maximum: 250 }
   validates :commentscounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :likescounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
