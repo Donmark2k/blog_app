4abce58e1d819353ae4f540fc9e5df7404beffc9
John Adibe <johnadibe123@gmail.com>
2023-05-19 10:51:11 -0800

    Add post comment counter decrement

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,8 @@ class Comment < ApplicationRecord
   def update_post_comment_counter
     post.increment!(:commentscounter)
   end
+
+  def decrement_post_comment_counter
+    post.decrement!(:commentscounter)
+  end
 end
