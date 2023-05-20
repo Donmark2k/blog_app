e8db46744695c99ce9bbd14945612e75ee51fd6b
Donmark2k <chukwuemekavp@gmail.com>
2023-05-20 06:29:51 +0100

    Add index routes in routrs.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,15 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
+
+  def index
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+    respond_to do |format|
+      format.json { render json: @comments }
+    end
+  end
+
   def new
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:post_id])
@@ -11,11 +21,16 @@ class CommentsController < ApplicationController
 
     @comment = @post.comments.new(comment_params)
     @comment.author = @user
-    if @comment.save
-      redirect_to request.referrer
-
-    else
-      render :new
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to request.referrer, notice: 'comment was successfully created.' }
+        format.json { render Json: @comment, status: :created }
+      else
+        format.html { render :new }
+        format.json do
+          render json: @comment, status: :created
+        end
+      end
     end
   end
 
